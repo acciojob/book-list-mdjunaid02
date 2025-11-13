79bfe3f6e695cd7444e22a1440293c1825370889
mdjunaid02 <156643108+mdjunaid02@users.noreply.github.com>
2025-11-13 10:48:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const title=document.getElementById("title").value;
+const Author=document.getElementById("author").value;
+const ISBN=document.getElementById("isbn").value;
+
+let table=document.getElementById("table-striped");
+
+if(document.getElementById("submit").onclick){
+	let newRow=table.insertRow();
+	let cell1=newRow.insertCell(title);
+	let cell2=newRow.insertCell(Author);
+	let cell3=newRow.insertCell(ISBN);
+}
