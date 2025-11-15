55a6b09945929d6ee7b1cbe9cca8216deae40b86
mdjunaid02 <156643108+mdjunaid02@users.noreply.github.com>
2025-11-15 19:48:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-//your JS code here. If required.
-const title=document.getElementById("title").value;
-const Author=document.getElementById("author").value;
-const ISBN=document.getElementById("isbn").value;
+document.getElementById("submit").addEventListener("click",function(e){
+	e.presendDefault();
+	const title=getElementById("title").value;
+	const author=getElmentById("author").value;
+	const isbn=getElementById("isbn").vaule;
 
-let table=document.getElementById("table-striped");
+	if(!author || !title || !isbn) return;
 
-if(document.getElementById("submit").onclick){
-	let newRow=table.insertRow();
-	let cell1=newRow.insertCell(title);
-	let cell2=newRow.insertCell(Author);
-	let cell3=newRow.insertCell(ISBN);
-}
+	const tableBody=document.getElementById("book-list");
+	const row=document.createElement("tr");
+
+	const titleCell=document.createElement("td");
+	titleCell.textContent=title;
+	row.appendChild(titleCell);
+
+	const authorCell=document.createElement("td");
+	authorCell.textContent=author;
+	row.appendChild(authorCell);
+
+	const isbnCell=document.createElement("td");
+	isbnCell.textContent=isbn;
+	row.appendChild(isbnCell);
+
+	tableBody.appendChild(row);
+});
